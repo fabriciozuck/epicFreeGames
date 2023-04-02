98aed6823f8a9e550fad210368859ec69e418f70
fabriciozuck <fabricioaraujo4000@gmail.com>
2023-04-02 15:01:16 -0300

    update

--- src/app.ts
+++ src/app.ts
@@ -5,15 +5,22 @@ import { ILocale } from './interfaces/locale_interface';
 import { ResultEpic } from './utils/resultEpic_mapper';
 import { HttpService } from './repositories/httpService';
 
+const API_BASE_URL = 'https://store-site-backend-static-ipv4.ak.epicgames.com';
+
 export async function getFreeGames(locale: ILocale) {
-    const api = `https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=${locale.language}&country=${locale.countryCode}&allowCountries=${locale.countryCode}`
-    const httpService = new HttpService;
-    const response = await httpService.get(api);
-    return response.reduce((previousValue: ResultEpic[], currentValue) => {
-        const resultEpic = ResultEpic.fromJSON(currentValue);
-        if (resultEpic.data.price.totalPrice.discountPrice == 0 && resultEpic.data.price.totalPrice.discount > 0) {
-            previousValue.push(resultEpic);
-        }
-        return previousValue;
-    }, []);
+    const api = `${API_BASE_URL}/freeGamesPromotions?locale=${locale.language}&country=${locale.countryCode}&allowCountries=${locale.countryCode}`
+    const httpService = new HttpService();
+    try {
+        const response = await httpService.get(api);
+        return response.reduce((previousValue: ResultEpic[], currentValue) => {
+            const resultEpic = ResultEpic.fromJSON(currentValue);
+            if (resultEpic.data.price.totalPrice.discount && resultEpic.data.price.totalPrice.discountPrice == 0) {
+                previousValue.push(resultEpic);
+            }
+            return previousValue;
+        }, []);
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
 }
